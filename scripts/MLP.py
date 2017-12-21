03e50aa284a09f9aefd706395ad2d2d11873dc5e
sarmishtaVelury <sarmishta.velury@gmail.com>
2017-12-21 15:09:41 +0530

    Modified Neural Net

--- scripts/MLP.py
+++ scripts/MLP.py
@@ -203,22 +203,24 @@ def fit(train, test, raw_train, raw_test, scaler, batch_size, nb_epoch, neurons)
     # fit model
     train_rmse, test_rmse = list(), list()
     for i in range(nb_epoch):
-        model.fit(X, y, epochs=1, batch_size=batch_size, verbose=0, shuffle=False)
+        model.fit(X, y, epochs=1, batch_size=batch_size, shuffle=False, verbose = True)
         # evaluate model on train data
         train_rmse.append(evaluate(model, raw_train, train, scaler, 0, batch_size))
         # evaluate model on test data
         test_rmse.append(evaluate(model, raw_test, test, scaler, 0, batch_size))
-    history = DataFrame()
+    history = pd.DataFrame()
     history['train'], history['test'] = train_rmse, test_rmse
-    return history
+    #predictions = model.predict(test, batch_size=1000)
+    #predictions = np.expm1(predictions).clip(lower=0.)
+    return history #,predictions
 
 # run diagnostic experiments
 def run():
     # config
-    repeats = 10
-    n_batch = 4
-    n_epochs = 1000
-    n_neurons = 500
+    repeats = 1
+    n_batch = 1000
+    n_epochs = 1
+    n_neurons = 10
     n_lag = 1
     # transform the scale of the data
     train_scaled, test_scaled = train,test
@@ -230,6 +232,59 @@ def run():
         pyplot.plot(history['test'], color='orange')
         print('%d) TrainRMSE=%f, TestRMSE=%f' % (i, history['train'].iloc[-1], history['test'].iloc[-1]))
     pyplot.savefig('diagnostic_epochs.png')
+    return predictions
+
+
+def sub(predictions):
+    sub1['id'] = test['id']
+    sub1['predictions'] = predictions
+    del train; del data;
+
+    # from hklee
+    # https://www.kaggle.com/zeemeen/weighted-mean-comparisons-lb-0-497-1st/code
+    dfs = { re.search('/([^/\.]*)\.csv', fn).group(1):
+        pd.read_csv(fn)for fn in glob.glob('../input/*.csv')}
+
+    for k, v in dfs.items(): locals()[k] = v
+
+    wkend_holidays = date_info.apply(
+        (lambda x:(x.day_of_week=='Sunday' or x.day_of_week=='Saturday') and x.holiday_flg==1), axis=1)
+    date_info.loc[wkend_holidays, 'holiday_flg'] = 0
+    date_info['weight'] = ((date_info.index + 1) / len(date_info)) ** 5  
+
+    visit_data = air_visit_data.merge(date_info, left_on='visit_date', right_on='calendar_date', how='left')
+    visit_data.drop('calendar_date', axis=1, inplace=True)
+    visit_data['visitors'] = visit_data.visitors.map(pd.np.log1p)
+
+    wmean = lambda x:( (x.weight * x.visitors).sum() / x.weight.sum() )
+    visitors = visit_data.groupby(['air_store_id', 'day_of_week', 'holiday_flg']).apply(wmean).reset_index()
+    visitors.rename(columns={0:'visitors'}, inplace=True) # cumbersome, should be better ways.
+
+    sample_submission['air_store_id'] = sample_submission.id.map(lambda x: '_'.join(x.split('_')[:-1]))
+    sample_submission['calendar_date'] = sample_submission.id.map(lambda x: x.split('_')[2])
+    sample_submission.drop('visitors', axis=1, inplace=True)
+    sample_submission = sample_submission.merge(date_info, on='calendar_date', how='left')
+    sample_submission = sample_submission.merge(visitors, on=[
+        'air_store_id', 'day_of_week', 'holiday_flg'], how='left')
+
+    missings = sample_submission.visitors.isnull()
+    sample_submission.loc[missings, 'visitors'] = sample_submission[missings].merge(
+        visitors[visitors.holiday_flg==0], on=('air_store_id', 'day_of_week'), 
+        how='left')['visitors_y'].values
+
+    missings = sample_submission.visitors.isnull()
+    sample_submission.loc[missings, 'visitors'] = sample_submission[missings].merge(
+        visitors[['air_store_id', 'visitors']].groupby('air_store_id').mean().reset_index(), 
+        on='air_store_id', how='left')['visitors_y'].values
+        
+    sample_submission['visitors'] = sample_submission.visitors.map(pd.np.expm1)
+    sub2 = sample_submission[['id', 'visitors']].copy()
+    sub_merge = pd.merge(sub1, sub2, on='id', how='inner')
+
+    sub_merge['visitors'] = (sub_merge['visitors_x'] + sub_merge['visitors_y'])/2
+    sub_merge[['id', 'visitors']].to_csv('submission.csv', index=False)
 
 
 run()
+
+#sub(predictions)
